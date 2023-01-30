fc1506bcc1202844d76af24388fa6678c8f03a4f
Daniel Palomino <dapalominop@gmail.com>
2023-01-29 23:27:16 -0600

    feat: added evaluation functions.

---
+++ src/workers/inference/utils/evaluation.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+def set_val_model(model: nn.Module):
+    """
+    """
+    
+    for k in model.state_dict().keys():
+        if 'conv5_pose' in k and 'weight' in k:
+            torch.nn.init.xavier_normal_(model.state_dict()[k])
+            print('filling xavier {}'.format(k))
+
+    for k in model.state_dict().keys():
+        if 'conv5_global' in k and 'weight' in k:
+            torch.nn.init.xavier_normal_(model.state_dict()[k])
+            print('filling xavier {}'.format(k))
+    model.eval()
+    
+    return model
+
+val_transform = A.Compose(
+        [A.Resize(224, 224), A.Normalize(mean=(0.485, 0.456, 0.406), 
+        std=(0.229, 0.224, 0.225)), ToTensorV2()]
+    )
